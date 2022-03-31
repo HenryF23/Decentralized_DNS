01b8a99ea970071f71b0a75b0565b8d120f20d62
Henry Fang <fanghaof@sfu.ca>
2022-03-30 23:20:51 -0700

    update dns.sol

--- dns.sol
+++ dns.sol
@@ -1,46 +1,52 @@
-pragma solidity 0.6.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 contract dns {
-    struct data {
+    struct ip_data {
         address owner;
-        string IP_address;
+        uint32 IP_address;
         bool initialized; // used to track if this key exist in the map
     }
 
-    mapping (string => data) records;
+    struct domain_data {
+        bytes32 domain;
+        bool initialized; // used to track if this key exist in the map
+    }
+
+    mapping (bytes32 => ip_data) records;
+    mapping (uint32 => domain_data) reverseRecords;
 
     // a modifier that make sure some functions can only be called by the owner of the domain
-    modifier onlyOwner(string memory domain) {
+    modifier onlyOwner(bytes32 domain) {
+        assert(records[domain].initialized == true);
         require(msg.sender == records[domain].owner);
         _;
     }
 
     // create a record
-    function createRecord(string memory domain, string memory IP) public returns (bool) {
-        if (records[domain].initialized) {
-            return false; // check if this domain has been created or not
-        }
-        records[domain] = data(msg.sender, IP, true);
-        return true;
+    function createRecord(bytes32 domain, uint32 IP) public {
+        require(records[domain].initialized == false);
+
+        records[domain] = ip_data(msg.sender, IP, true);
+        reverseRecords[IP] = domain_data(domain, true);
     }
 
-    function getRecord(string memory domain) public view returns (string memory) {
+    function getRecord(bytes32 domain) public view returns (uint32) {
+        assert(records[domain].initialized);
         return records[domain].IP_address;
+    }
+    
+    function getDomain(uint32 IP) public view returns (bytes32) {
+        assert(reverseRecords[IP].initialized);
+        return reverseRecords[IP].domain;
     }
 
     // untested
-    function transferDomain(string memory domain, address userAddress) public onlyOwner(domain) returns(bool){
+    function transferDomain(bytes32 domain, address userAddress) public onlyOwner(domain) returns(bool){
         records[domain].owner = userAddress;
         return true;
     }
 
-    function updateRecord(string memory domain, string memory newIP) public onlyOwner(domain) returns(bool) {
-        if (records[domain].initialized) {
-            return false; // check if this domain has been created or not
-        }
-        require(msg.sender == records[domain].owner);
-
+    function updateRecord(bytes32 domain, uint32 newIP) public onlyOwner(domain) {
         records[domain].IP_address = newIP;
-        return true;
     }
 }
